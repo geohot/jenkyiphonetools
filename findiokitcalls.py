9d53c30ffea5e33b037e46dd407df1f35c557dc1
geohot <geohot@linuxpresent.(none)>
2013-12-02 01:25:02 -0500

    fuzzy

--- findiokitcalls.py
+++ findiokitcalls.py
@@ -20,7 +20,7 @@ def search_file(f):
       cnt += 2 * (fxn > 0x80000000 and fxn < 0x81000000)
       cnt += (flag < 20) or (flag >= 4294967295)
       cnt += (i < 0x2000 or i == 0xFFFFFFFF)
-      cnt += (un < 10)
+      cnt += (un < 20) or (un >= 4294967295)
       cnt += (o < 0x2000 or o == 0xFFFFFFFF)
       #cnt += (rc > 0)
       if cnt >= 6:
@@ -34,24 +34,34 @@ def search_file(f):
           iostart = iolastreset
         rc = 0
 
-  if iolen >= 3:
+  if iolen >= 2:
     print "in",f
     print "start",iostart,"len",iolen
 
     ii = []
     oo = []
+    iic = []
+    ooc = []
 
     for addr in range(iostart, iostart+iolen*0x14, 0x14):
-      (fxn, flag, i, un, o) = struct.unpack("Iiiii", d[addr:addr+0x14])
-      print "0x%X %3d %4d %3d %4d" % (fxn, flag, i, un, o)
-      ii.append(hex(i))
-      oo.append(hex(o))
+      (fxn, ic, i, oc, o) = struct.unpack("Iiiii", d[addr:addr+0x14])
+      print "0x%X %3d %4d %3d %4d" % (fxn, ic, i, oc, o)
+      iic.append(ic)
+      ooc.append(oc)
+      ii.append(i)
+      oo.append(o)
     
+    iic = map(str, iic)
+    ooc = map(str, ooc)
+    ii = map(str, ii)
+    oo = map(str, oo)
     svcname = '"'+f.split("/")[-1].split(".")[0]+'"'
     svcnames.append(svcname)
     print '#define SERVICENAME '+svcname
     print "int ii[] = {"+','.join(ii)+"};"
     print "int oo[] = {"+','.join(oo)+"};"
+    print "int iic[] = {"+','.join(iic)+"};"
+    print "int ooc[] = {"+','.join(ooc)+"};"
 
 for f in sys.argv[1:]:
   search_file(f)
